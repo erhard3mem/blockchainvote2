33a2274ab0b117300b05b9b4ab37bd61e424a2f6
Erhard <erhard3mem@gmail.com>
2024-10-25 18:14:10 +0200

    db path

--- index.js
+++ index.js
@@ -131,8 +131,8 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = 'bl0ckchainv0te'
 
 // think about: is this a good idea?
-//const db_bc = new sqlite3.Database('/var/data/bc.db', (err) => {
-const db_bc = new sqlite3.Database('bc.db', (err) => {
+const db_bc = new sqlite3.Database('/var/data/bc.db', (err) => {
+//const db_bc = new sqlite3.Database('bc.db', (err) => {
   if (err) {
     console.error(err.message);
   }
@@ -153,8 +153,8 @@ const db_bc = new sqlite3.Database('bc.db', (err) => {
 
 });
 
-//const db_user = new sqlite3.Database('/var/data/user.db', (err) => {
-const db_user = new sqlite3.Database('user.db', (err) => {
+const db_user = new sqlite3.Database('/var/data/user.db', (err) => {
+//const db_user = new sqlite3.Database('user.db', (err) => {
   if (err) {
     console.error(err.message);
   }
@@ -173,8 +173,8 @@ const db_user = new sqlite3.Database('user.db', (err) => {
 });
 
 // Database connection
-//const db = new sqlite3.Database('/var/data/main.db', (err) => {
-const db = new sqlite3.Database('main.db', (err) => {
+const db = new sqlite3.Database('/var/data/main.db', (err) => {
+//const db = new sqlite3.Database('main.db', (err) => {
   if (err) {
     console.error(err.message);
   }
